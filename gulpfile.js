7029240060b81d7339fb6f29d373a3c36e4916fb
Evgeny <yauheni.aliakseyenko@neklo.com>
2025-07-03 20:48:03 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass")(require("sass"));
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require('browser-sync').create();
 
 gulp.task("scss", function () {
   return gulp
     .src("./scss/**/*.scss")
     .pipe(sass.sync().on("error", sass.logError))
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("watch", function () {
-  gulp.watch("scss/**/*.scss", gulp.series("scss"));
+gulp.task('watch', function() {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./scss/*.scss", gulp.series("scss"));
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+// gulp.task("watch", function () {
+//   gulp.watch("scss/**/*.scss", gulp.series("scss"));
+// });
