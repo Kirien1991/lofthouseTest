5bb421559bec494026be78d46622b86e767d329f
Kirien1991 <162041026+Kirien1991@users.noreply.github.com>
2025-07-04 13:57:12 +0300

    commit4

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const browserSync = require('browser-sync').create();
+const browserSync = require("browser-sync").create();
 
 gulp.task("scss", function () {
   return gulp
-    .src("./scss/**/*.scss")
+    .src("./scss/**/**/*.scss")
     .pipe(sass.sync().on("error", sass.logError))
     .pipe(gulp.dest("./css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
+gulp.task("watch", function () {
   browserSync.init({
-    server: "./"
+    server: "./",
   });
 
-  gulp.watch("./scss/*.scss", gulp.series("scss"));
-  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./scss/**/**/*.scss", gulp.series("scss"));
+  gulp.watch("./*.html").on("change", browserSync.reload);
 });
 
 // gulp.task("watch", function () {
