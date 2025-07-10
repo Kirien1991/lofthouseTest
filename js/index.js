944d44614220a5b70dec7e5f1fec5de1dd66df5e
Kirien1991 <162041026+Kirien1991@users.noreply.github.com>
2025-07-10 16:05:58 +0300

    commit34

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 //burger
 const burgerBtn = document.getElementById("burger");
 const menu = document.getElementById("menu");
+const body = document.body;
 
 burgerBtn.addEventListener("click", () => {
   burgerBtn.classList.toggle("active");
   menu.classList.toggle("active");
+  body.classList.toggle("no-scroll");
 });
