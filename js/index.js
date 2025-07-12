e5b4741acb45e75ad53619dee80b48f0002da343
Kirien1991 <162041026+Kirien1991@users.noreply.github.com>
2025-07-12 16:15:06 +0300

    commit37

--- js/index.js
+++ js/index.js
@@ -5,4 +5,18 @@ const menu = document.getElementById("menu");
 burgerBtn.addEventListener("click", () => {
   burgerBtn.classList.toggle("active");
   menu.classList.toggle("active");
+  document.body.classList.toggle("menu_mobile_didden");
+});
+
+//phone mask
+mask("[data-tel-input]");
+
+const phoneInputs = document.querySelectorAll("[data-tel-input]");
+phoneInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value == "+") input.value = "";
+  });
+  input.addEventListener("blur", () => {
+    if (input.value == "+") input.value = "";
+  });
 });
